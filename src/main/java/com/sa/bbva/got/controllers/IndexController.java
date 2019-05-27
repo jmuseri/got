8fcb0e31e6886d122214b537f7218eb973cb1534
Sistemas <soporte@sistemasactivos.com>
2019-05-27 16:34:40 -0300

    Json Response updated

--- src/main/java/com/sa/bbva/got/controllers/IndexController.java
+++ src/main/java/com/sa/bbva/got/controllers/IndexController.java
@@ -3,12 +3,16 @@ package com.sa.bbva.got.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
 @RequestMapping("/")
 public class IndexController {
+
     @RequestMapping(method = RequestMethod.GET)
-    String index() {
-        return "index";
+    public RedirectView index() {
+        RedirectView redirectView = new RedirectView();
+        redirectView.setUrl("/swagger-ui.html");
+        return redirectView;
     }
 }
--- src/main/java/com/sa/bbva/got/controllers/ProductController.java
+++ src/main/java/com/sa/bbva/got/controllers/ProductController.java
@@ -1,5 +1,6 @@
 package com.sa.bbva.got.controllers;
 
+import com.sa.bbva.got.bean.StatusResponse;
 import com.sa.bbva.got.model.Product;
 import com.sa.bbva.got.services.ProductService;
 import io.swagger.annotations.Api;
@@ -11,13 +12,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 @RequestMapping("/product")
-@Api(value = "onlinestore", description = "Operations pertaining to products in Online Store")
+@Api(value = "functional", description = "Functional Operations in GOT")
 public class ProductController {
 
     private ProductService productService;
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     public void setProductService(ProductService productService) {
@@ -30,43 +34,84 @@ public class ProductController {
             @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
             @ApiResponse(code = 404, message = "The resource you were trying to reach is not found") })
     @RequestMapping(value = "/list", method = RequestMethod.GET, produces = "application/json")
-    public Iterable<Product> list(Model model) {
-        Iterable<Product> productList = productService.listAllProducts();
-        return productList;
+    public ResponseEntity<?> list(Model model) {
+        try {
+            Iterable<Product> productList = productService.listAllProducts();
+            ResponseEntity<?> response = new ResponseEntity<>(productList, HttpStatus.OK);
+            return response;
+        } catch (Exception e) {
+            logger.error("", e);
+            StatusResponse statusResponse = new StatusResponse("error", "Exception Error", e.getMessage());
+            ResponseEntity<?> response = new ResponseEntity<>(statusResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+            return response;
+        }
     }
 
     @ApiOperation(value = "Search a product with an ID", response = Product.class)
     @RequestMapping(value = "/show/{id}", method = RequestMethod.GET, produces = "application/json")
-    public Product showProduct(@PathVariable Integer id, Model model) {
-        Product product = productService.getProductById(id);
-        return product;
+    public ResponseEntity<?> showProduct(@PathVariable Integer id, Model model) {
+        try {
+            Product product = productService.getProductById(id);
+            ResponseEntity<?> response = new ResponseEntity<>(product, HttpStatus.OK);
+            return response;
+        } catch (Exception e) {
+            logger.error("", e);
+            StatusResponse statusResponse = new StatusResponse("error", "Exception Error", e.getMessage());
+            ResponseEntity<?> response = new ResponseEntity<>(statusResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+            return response;
+        }
     }
 
     @ApiOperation(value = "Add a product")
     @RequestMapping(value = "/add", method = RequestMethod.POST, produces = "application/json")
     public ResponseEntity<?> saveProduct(@RequestBody Product product) {
-        productService.saveProduct(product);
-        ResponseEntity<?> response = new ResponseEntity<>("Product saved successfully", HttpStatus.OK);
-        return response;
+        try {
+            productService.saveProduct(product);
+            StatusResponse status = new StatusResponse("ok", "Product saved successfully", null);
+            ResponseEntity<?> response = new ResponseEntity<>(status, HttpStatus.OK);
+            return response;
+        } catch (Exception e) {
+            logger.error("", e);
+            StatusResponse statusResponse = new StatusResponse("error", "Product not saved", e.getMessage());
+            ResponseEntity<?> response = new ResponseEntity<>(statusResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+            return response;
+        }
     }
 
     @ApiOperation(value = "Update a product")
     @RequestMapping(value = "/update/{id}", method = RequestMethod.PUT, produces = "application/json")
     public ResponseEntity<?> updateProduct(@PathVariable Integer id, @RequestBody Product product) {
-        Product storedProduct = productService.getProductById(id);
-        storedProduct.setDescription(product.getDescription());
-        storedProduct.setImageUrl(product.getImageUrl());
-        storedProduct.setPrice(product.getPrice());
-        productService.saveProduct(storedProduct);
-        return new ResponseEntity<>("Product updated successfully", HttpStatus.OK);
+        try {
+            Product storedProduct = productService.getProductById(id);
+            storedProduct.setDescription(product.getDescription());
+            storedProduct.setImageUrl(product.getImageUrl());
+            storedProduct.setPrice(product.getPrice());
+            productService.saveProduct(storedProduct);
+            StatusResponse status = new StatusResponse("ok", "Product updated successfully", null);
+            ResponseEntity<?> response = new ResponseEntity<>(status, HttpStatus.OK);
+            return response;
+        } catch (Exception e) {
+            logger.error("", e);
+            StatusResponse statusResponse = new StatusResponse("error", "Product not saved", e.getMessage());
+            ResponseEntity<?> response = new ResponseEntity<>(statusResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+            return response;
+        }
     }
 
     @ApiOperation(value = "Delete a product")
     @RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE, produces = "application/json")
     public ResponseEntity<?> delete(@PathVariable Integer id) {
-        productService.deleteProduct(id);
-        return new ResponseEntity<>("Product deleted successfully", HttpStatus.OK);
-
+        try {
+            productService.deleteProduct(id);
+            StatusResponse status = new StatusResponse("ok", "Product deleted successfully", null);
+            ResponseEntity<?> response = new ResponseEntity<>(status, HttpStatus.OK);
+            return response;
+        } catch (Exception e) {
+            logger.error("", e);
+            StatusResponse statusResponse = new StatusResponse("error", "Product not deleted", e.getMessage());
+            ResponseEntity<?> response = new ResponseEntity<>(statusResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+            return response;
+        }
     }
 
 }
