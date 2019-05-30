789a4a31c554e6676b005638b16acbb1092fdcbb
German <soporte@sistemasactivos.com>
2019-05-30 13:21:10 -0300

    Configuration folder changed

--- src/main/java/com/sa/bbva/got/configuration/SwaggerConfig.java
+++ src/main/java/com/sa/bbva/got/configuration/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.sa.bbva.got.config;
+package com.sa.bbva.got.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
