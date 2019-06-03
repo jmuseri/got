a87bc2331235ad7b9e9c02e9ea8a61d670f5ef2e
Sistemas <soporte@sistemasactivos.com>
2019-06-03 18:23:50 -0300

    Test updated

--- src/test/java/com/sa/bbva/got/GotApplicationTests.java
+++ src/test/java/com/sa/bbva/got/GotApplicationTests.java
@@ -2,12 +2,14 @@ package com.sa.bbva.got;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = GotApplication.class)
+@DataJpaTest
 @WebAppConfiguration
 public class GotApplicationTests {
 
