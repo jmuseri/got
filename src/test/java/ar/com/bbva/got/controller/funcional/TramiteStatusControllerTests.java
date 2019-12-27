49090a123dfb56c1c5e5cd3e01b359964726a69c
Facundo Serna <fserna@baufest.com>
2019-12-27 10:18:53 -0300

    Update TramiteStatusControllerTests.java

--- src/test/java/ar/com/bbva/got/controller/funcional/TramiteStatusControllerTests.java
+++ src/test/java/ar/com/bbva/got/controller/funcional/TramiteStatusControllerTests.java
@@ -1,5 +1,8 @@
 package ar.com.bbva.got.controller.funcional;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -17,9 +20,11 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import ar.com.bbva.got.model.Tramite;
-import ar.com.bbva.got.model.TramiteDetalle;
+import ar.com.bbva.got.model.TramiteAutorizado;
+import ar.com.bbva.got.model.TramiteAutorizadoKey;
 import ar.com.bbva.got.service.funcional.MotivoRechazoService;
 import ar.com.bbva.got.service.funcional.TramiteService;
+
 @RunWith(SpringRunner.class)
 @WebMvcTest(TramiteStatusController.class)
 public class TramiteStatusControllerTests {
@@ -39,9 +44,32 @@ public class TramiteStatusControllerTests {
 	public static Tramite tramite = new Tramite();
 	
 	@Before
-	public void setUp() {
+	public void setUp() {	
+		
+		Set<TramiteAutorizado> autorizados = new HashSet<TramiteAutorizado>();
+		
+		TramiteAutorizado autorizado1 = new TramiteAutorizado();
+		TramiteAutorizadoKey id1 = new TramiteAutorizadoKey();
+		id1.setAutorizadoId(1);
+		autorizado1.setId(id1);
+		
+		TramiteAutorizado autorizado2 = new TramiteAutorizado();
+		TramiteAutorizadoKey id2 = new TramiteAutorizadoKey();
+		id2.setAutorizadoId(2);
+		autorizado2.setId(id2);
+		
+		TramiteAutorizado autorizado3 = new TramiteAutorizado();
+		TramiteAutorizadoKey id3 = new TramiteAutorizadoKey();
+		id3.setAutorizadoId(3);
+		autorizado3.setId(id3);
+				
+		autorizados.add(autorizado1);
+		autorizados.add(autorizado2);
+		autorizados.add(autorizado3);		
+		
 		tramite.setId(0);
 		tramite.setUsuModif("Ana");
+		tramite.setAutorizado(autorizados);
 	}
 	
 	@Test
@@ -92,14 +120,51 @@ public class TramiteStatusControllerTests {
 	}
 	
 	@Test
-	public void finalizarTramiteUsuarioInvalidoTest() throws Exception {
+	public void finalizarTramiteUsuarioNullTest() throws Exception {
 		Integer id = 0;
 		
 		Mockito.when(tramiteService.getById(id)).thenReturn(tramite);
 		
 		mockMvc.perform(MockMvcRequestBuilders.post("/funcional/tramite/" + id.toString() + "/finalizar").accept(MediaType.APPLICATION_JSON))
-		.andExpect(MockMvcResultMatchers.status().is5xxServerError());
+		.andExpect(MockMvcResultMatchers.status().is4xxClientError());
+	}
+	
+	@Test
+	public void finalizarTramiteUsuarioInvalidoTest() throws Exception {
+		Integer id = 0;
 		
-		// TODO terminar
+		Mockito.when(tramiteService.getById(id)).thenReturn(tramite);
+		
+		mockMvc.perform(MockMvcRequestBuilders.post("/funcional/tramite/" + id.toString() + "/finalizar?usuario=Pepe").accept(MediaType.APPLICATION_JSON))
+		.andExpect(MockMvcResultMatchers.status().is4xxClientError());
+	}
+	
+	@Test
+	public void finalizarTramiteExceptionTest() throws Exception {
+		Integer id = 0;
+		
+		Mockito.when(tramiteService.getById(id)).thenReturn(tramite);
+		
+		Mockito.when(tramiteService.save(Mockito.any(Tramite.class))).thenThrow(NullPointerException.class);
+		
+		mockMvc.perform(MockMvcRequestBuilders.post("/funcional/tramite/" + id.toString() + "/finalizar?usuario=Ana").accept(MediaType.APPLICATION_JSON))
+		.andExpect(MockMvcResultMatchers.status().is5xxServerError());
+	}
+	
+	@Test
+	public void finalizarTramiteConAutorizadoTest() throws Exception {
+		Integer id = 0;
+		
+		Mockito.when(tramiteService.getById(id)).thenReturn(tramite);
+		
+		mockMvc.perform(MockMvcRequestBuilders.post("/funcional/tramite/" + id.toString() + "/finalizar?usuario=Ana&idAutorizado=1").accept(MediaType.APPLICATION_JSON))
+		.andExpect(MockMvcResultMatchers.status().isOk());
+		
+		ArgumentCaptor<Integer> argumentCaptor = ArgumentCaptor.forClass(Integer.class);
+		ArgumentCaptor<Tramite> argumentCaptor2 = ArgumentCaptor.forClass(Tramite.class);
+		
+		Mockito.verify(tramiteService).getById(argumentCaptor.capture());
+		Assert.assertEquals(id, argumentCaptor.getValue());
+		Mockito.verify(tramiteService).save(argumentCaptor2.capture());	
 	}
 }
