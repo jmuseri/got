8e0b72ff9b9b3dcc9ef5214100690e1b612d8c1e
Facundo Serna <fserna@baufest.com>
2020-01-08 12:27:29 -0300

    TramiteStatusController 100% coverage

--- src/test/java/ar/com/bbva/got/controller/funcional/TramiteStatusControllerTests.java
+++ src/test/java/ar/com/bbva/got/controller/funcional/TramiteStatusControllerTests.java
@@ -19,6 +19,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import ar.com.bbva.got.model.MotivoRechazo;
 import ar.com.bbva.got.model.Tramite;
 import ar.com.bbva.got.model.TramiteAutorizado;
 import ar.com.bbva.got.model.TramiteAutorizadoKey;
@@ -42,6 +43,8 @@ public class TramiteStatusControllerTests {
 	private TramiteStatusController tramiteStatusController;
 	
 	public static Tramite tramite = new Tramite();
+	
+	public static MotivoRechazo motivoRechazo = new MotivoRechazo();
 	
 	@Before
 	public void setUp() {	
@@ -70,6 +73,7 @@ public class TramiteStatusControllerTests {
 		tramite.setId(0);
 		tramite.setUsuModif("Ana");
 		tramite.setAutorizado(autorizados);
+				
 	}
 	
 	@Test
@@ -197,6 +201,124 @@ public class TramiteStatusControllerTests {
 		Mockito.when(tramiteService.save(Mockito.any(Tramite.class))).thenThrow(NullPointerException.class);
 		
 		mockMvc.perform(MockMvcRequestBuilders.post("/funcional/tramite/" + id.toString() + "/cancelar?usuario=" + usuario).accept(MediaType.APPLICATION_JSON))
+		.andExpect(MockMvcResultMatchers.status().is5xxServerError());
+	}
+	
+	@Test
+	public void rechazarTramiteTest() throws Exception {
+		Integer id = 0;
+		Integer motivoRechazoId = 0;
+		String usuario = "Ana";
+		
+		Mockito.when(tramiteService.getById(id)).thenReturn(tramite);
+		Mockito.when(motivoRechazoService.getById(motivoRechazoId)).thenReturn(motivoRechazo);
+		
+		mockMvc.perform(MockMvcRequestBuilders.post("/funcional/tramite/" + id.toString() + "/rechazar?motivoRechazoId=" + motivoRechazoId.toString() + "&usuario=" + usuario).accept(MediaType.APPLICATION_JSON))
+		.andExpect(MockMvcResultMatchers.status().isOk());
+		
+		ArgumentCaptor<Integer> argumentCaptor = ArgumentCaptor.forClass(Integer.class);
+		ArgumentCaptor<Integer> argumentCaptor2 = ArgumentCaptor.forClass(Integer.class);		
+		ArgumentCaptor<Tramite> argumentCaptor3 = ArgumentCaptor.forClass(Tramite.class);
+		
+		
+		Mockito.verify(tramiteService).getById(argumentCaptor.capture());
+		Assert.assertEquals(id, argumentCaptor.getValue());
+		Mockito.verify(motivoRechazoService).getById(argumentCaptor2.capture());
+		Assert.assertEquals(motivoRechazoId, argumentCaptor.getValue());
+		Mockito.verify(tramiteService).save(argumentCaptor3.capture());
+		Assert.assertEquals(usuario, argumentCaptor3.getValue().getUsuModif());
+		Assert.assertEquals(motivoRechazo, argumentCaptor3.getValue().getMotivoRechazo());
+	}
+	
+	@Test
+	public void rechazarTramiteSinMotivoRechazoTest() throws Exception {
+		Integer id = 0;
+		String usuario = "Ana";
+		
+		Mockito.when(tramiteService.getById(id)).thenReturn(tramite);
+		
+		mockMvc.perform(MockMvcRequestBuilders.post("/funcional/tramite/" + id.toString() + "/rechazar?usuario=" + usuario).accept(MediaType.APPLICATION_JSON))
+		.andExpect(MockMvcResultMatchers.status().isOk());
+		
+		ArgumentCaptor<Integer> argumentCaptor = ArgumentCaptor.forClass(Integer.class);	
+		ArgumentCaptor<Tramite> argumentCaptor2 = ArgumentCaptor.forClass(Tramite.class);
+		
+		
+		Mockito.verify(tramiteService).getById(argumentCaptor.capture());
+		Assert.assertEquals(id, argumentCaptor.getValue());
+		Mockito.verify(tramiteService).save(argumentCaptor2.capture());
+		Assert.assertEquals(usuario, argumentCaptor2.getValue().getUsuModif());
+		Assert.assertEquals(null, argumentCaptor2.getValue().getMotivoRechazo());
+		Mockito.verify(motivoRechazoService, Mockito.times(0)).getById(Mockito.any());
+	}
+	
+	@Test
+	public void rechazarTramiteExceptionTest() throws Exception {
+		Integer id = 0;
+		String usuario = "Ana";
+		
+		Mockito.when(tramiteService.getById(id)).thenReturn(null);
+		
+		Mockito.when(tramiteService.save(Mockito.any(Tramite.class))).thenThrow(NullPointerException.class);
+		
+		mockMvc.perform(MockMvcRequestBuilders.post("/funcional/tramite/" + id.toString() + "/rechazar?usuario=" + usuario).accept(MediaType.APPLICATION_JSON))
+		.andExpect(MockMvcResultMatchers.status().is5xxServerError());
+	}
+	
+	@Test
+	public void activarTramiteTest() throws Exception {
+		Integer id = 0;
+		String usuario = "Ana";
+		
+		Mockito.when(tramiteService.getById(id)).thenReturn(tramite);
+		
+		mockMvc.perform(MockMvcRequestBuilders.post("/funcional/tramite/" + id.toString() + "/activar?usuario=" + usuario).accept(MediaType.APPLICATION_JSON))
+		.andExpect(MockMvcResultMatchers.status().isOk());
+		
+		ArgumentCaptor<Integer> argumentCaptor = ArgumentCaptor.forClass(Integer.class);	
+		ArgumentCaptor<Tramite> argumentCaptor2 = ArgumentCaptor.forClass(Tramite.class);
+		
+		
+		Mockito.verify(tramiteService).getById(argumentCaptor.capture());
+		Assert.assertEquals(id, argumentCaptor.getValue());
+		Mockito.verify(tramiteService).save(argumentCaptor2.capture());
+		Assert.assertEquals(usuario, argumentCaptor2.getValue().getUsuModif());
+	}
+	
+	@Test
+	public void activarTramiteExceptionTest() throws Exception {
+		Integer id = 0;
+		String usuario = "Ana";
+		
+		Mockito.when(tramiteService.getById(id)).thenReturn(null);
+		
+		Mockito.when(tramiteService.save(Mockito.any(Tramite.class))).thenThrow(NullPointerException.class);
+		
+		mockMvc.perform(MockMvcRequestBuilders.post("/funcional/tramite/" + id.toString() + "/activar?usuario=" + usuario).accept(MediaType.APPLICATION_JSON))
+		.andExpect(MockMvcResultMatchers.status().is5xxServerError());
+	}
+	
+	@Test
+	public void deleteTramiteTest() throws Exception {
+		Integer id = 0;
+		
+		mockMvc.perform(MockMvcRequestBuilders.post("/funcional/tramite/" + id.toString() + "/eliminar").accept(MediaType.APPLICATION_JSON))
+		.andExpect(MockMvcResultMatchers.status().isOk());
+		
+		ArgumentCaptor<Integer> argumentCaptor = ArgumentCaptor.forClass(Integer.class);	
+				
+		Mockito.verify(tramiteService).delete(argumentCaptor.capture());
+		Assert.assertEquals(id, argumentCaptor.getValue());
+
+	}
+	
+	@Test
+	public void deleteTramiteExceptionTest() throws Exception {
+		Integer id = 0;
+		
+		Mockito.doThrow(NullPointerException.class).when(tramiteService).delete(id);
+		
+		mockMvc.perform(MockMvcRequestBuilders.post("/funcional/tramite/" + id.toString() + "/eliminar").accept(MediaType.APPLICATION_JSON))
 		.andExpect(MockMvcResultMatchers.status().is5xxServerError());
 	}
 }
